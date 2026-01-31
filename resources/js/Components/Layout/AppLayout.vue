f73c1bd74d29e0f93e5cf46048f390a71ad26be4
hasibul alam <hasibulalam108@gmail.com>
2026-02-01 01:38:55 +0600

    create AppLayout and basic Home page

---
+++ resources/js/Components/Layout/AppLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<template>
+    <div class="main-h-screen bg-gray-900 text-white">
+        <div class="h-20 bg-gray-800 flex items-center justify-center">
+            <p class="text-gray-400">navbar is comming</p>
+        </div>
+
+       <!-- main Content -->
+        <main >
+            <slot />
+        </main>
+
+        <!-- footer -->
+         <div class="py-12 bg-gray-800 text-center">
+       <p class="text-gray-400">Footer is coming</p>
+
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AppLayout from '@/Components/Layout/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout>
+        <div class="container mx-auto px-4 py-20 text-center">
+            <h1 class="text-6xl font-bold mb-4">
+                <span class="bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 bg-clip-text text-transparent">
+                    Hasibul Alam
+                </span>
+            </h1>
+            <p class="text-2xl text-gray-300 mb-6">Full Stack Developer</p>
+
+        </div>
+
+    </AppLayout>
+</template>
